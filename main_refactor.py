50b1972a0c4aad8c075494890d2556ecaac7e728
jong <jstebner007@gmail.com>
2023-05-02 17:28:07 -0400

    reduced all but main

--- main_refactor.py
+++ main_refactor.py
@@ -1,54 +1,26 @@
-# Define the board as a list of lists
-# board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-
-# Function to print the board
 def print_board(board):
-    print('\n'.join([' '.join(row) for row in board]))
+    print('\n\nThis is the current board state:\n'+'\n'.join([' '.join(board[3*i:3*(i+1)]) for i in range(3)]))
 
-# Function to check if the game is over
 def is_game_over(board):
-    # Check for a winner
-    for i in range(3):
-        if board[i][0] != '-' and board[i][0] == board[i][1] == board[i][2]:
-            return True
-        if board[0][i] != '-' and board[0][i] == board[1][i] == board[2][i]:
-            return True
-    if board[0][0] != '-' and board[0][0] == board[1][1] == board[2][2]:
-        return True
-    if board[0][2] != '-' and board[0][2] == board[1][1] == board[2][0]:
-        return True
+    return True if any([(board[3*i] != '-' and board[3*i] == board[3*i + 1] == board[3*i + 2]) or (board[i] != '-' and board[i] == board[3+i] == board[6+i]) for i in range(3)]) or (board[0] != '-' and board[0] == board[4] == board[8]) or (board[2] != '-' and board[2] == board[4] == board[6]) else '-' not in board
 
-    # Check for a tie
-    for row in board:
-        for cell in row:
-            if cell == '-':
-                return False
-    return True
+def get_move(board):
+    return (lambda f: lambda a: f(f, a))(lambda rec, move: [print('Invalid move.'), rec(rec, int(input("\nChoose your move: ")))][1] if (move < 0 or move > 8) or (board[move] != '-') else move)(int(input("\nChoose your move: ")))
 
-# Function to get the next move from the player
-def get_move(player):
-    while True:
-        move = int(input("Choose your move: "))
-        row, col = move//3, move%3
-        if row < 0 or row > 2 or col < 0 or col > 2:
-            print("Invalid move.")
-        elif board[row][col] != '-':
-            print("Invalid move.")
-        else:
-            return row, col
-
-# Play the game
-print_board(board)
-player = 'X'
-while not is_game_over(board):
-    row, col = get_move(player)
-    board[row][col] = player
+def main(board):
     print_board(board)
-    if is_game_over(board):
-        print("Player " + player + " wins!")
-    elif player == 'X':
-        player = 'O'
-    else:
-        player = 'X'
-print("Game over.")
+    player = 'X'
+    while not is_game_over(board):
+        move = get_move(board)
+        board[move] = player
+        print_board(board)
+        if is_game_over(board):
+            # print("Player " + player + " wins!")
+            pass
+        elif player == 'X':
+            player = 'O'
+        else:
+            player = 'X'
+    print("Game over.")
+    
+main(['-', '-', '-', '-', '-', '-', '-', '-', '-'])
